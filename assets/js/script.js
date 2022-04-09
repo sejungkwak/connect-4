e66468a9379e80ab302a29496c912f12cab7b03a
Sejung Kwak <kwak.sejung@gmail.com>
2022-04-09 21:16:01 +0000

    feat: Add event listeners for button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,6 +26,111 @@ const leaderboardSection = elById('leaderboard');
 const contactSection = elById('contact');
 const successSection = elById('success');
 
+// Button Click EventListeners
+
+// logoLink => opens settingsSection, closes other sections
+logoLink.addEventListener('click', () => {
+  openSection('settings');
+  closeSection();
+});
+
+// navNewGameBtn => opens settingsSection, closes other sections except gameSection if active
+navNewGameBtn.addEventListener('click', () => {
+  openSection('settings');
+  closeSection();
+});
+
+// navHelpBtn => opens helpSection, closes other sections except gameSection if active
+navHelpBtn.addEventListener('click', () => {
+  openSection('help');
+  closeSection();
+});
+
+// navLeaderboardBtn => opens leaderboardSection, closes other sections except gameSection if active
+navLeaderboardBtn.addEventListener('click', () => {
+  openSection('leaderboard');
+  closeSection();
+});
+
+// settingStartBtn => oepns gameSection, closes other sections
+settingStartBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  openSection('game');
+  closeSection();
+});
+
+// settingCloseBtn => closes settingsSection
+settingCloseBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  closeSection();
+});
+
+// soundBtn => turns on sound effects
+soundBtn.addEventListener('click', soundBtnHander);
+
+// muteBtn => turns off sound effects
+muteBtn.addEventListener('click', soundBtnHander);
+
+// helpCloseBtn => closes helpSection
+helpCloseBtn.addEventListener('click', closeSection);
+
+// leaderboardDeleteBtn => deletes local storage data
+leaderboardDeleteBtn.addEventListener('click', deleteData);
+
+// leaderboardCloseBtn => closes leaderboardSection
+leaderboardCloseBtn.addEventListener('click', closeSection);
+
+// contactSendBtn => sends message, opens successSection, closes contact section
+contactSendBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  sendMessage();
+  openSection('success');
+  closeSection();
+});
+
+// contactCloseBtn => closes contactSection
+contactCloseBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  closeSection();
+});
+
+// successHomeBtn => closes successSection, opens settingsSection
+successHomeBtn.addEventListener('click', () => {
+  openSection('settings');
+  closeSection();
+});
+
+// footerContactBtn => opens contactSection, closes other sections except gameSection if active
+footerContactBtn.addEventListener('click', () => {
+  openSection('contact');
+  closeSection();
+});
+
+// Opens a section
+function openSection(name) {
+
+}
+
+// closes all section except game board section
+function closeSection() {
+
+}
+
+// Sound button click handler
+function soundBtnHander() {
+
+}
+
+// Deletes Local storage data
+function deleteData() {
+
+}
+
+// Sends user message
+function sendMessage() {
+
+}
+
 /* 
 Helper functions
 Source: Web Dev Simplified's "Stop Wasting Your Time - Use These 16 JS Utility Functions Instead"(https://www.youtube.com/watch?v=EoUIS2PxKCs&t=202s)
