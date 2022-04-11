396231173b6401258be560403d2a68df62aca229
Sejung Kwak <kwak.sejung@gmail.com>
2022-04-11 23:39:28 +0000

    feat: Add code to configure a new game | script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Get button elements
+// Button elements
 const logoLink = elById('logoLink');
 const navNewGameBtn = elById('primaryNewGame');
 const navHelpBtn = elById('primaryHelp');
@@ -18,7 +18,7 @@ const contactCloseBtn = elById('contactCloseBtn');
 const successHomeBtn = elById('successHomeBtn');
 const footerContactBtn = elById('footerContactBtn');
 
-// Get section elements
+// Section elements
 const sections = qsa('.section');
 const settingsSection = elById('settings');
 const gameSection = elById('board');
@@ -27,7 +27,7 @@ const leaderboardSection = elById('leaderboard');
 const contactSection = elById('contact');
 const successSection = elById('success');
 
-// Get checkbox input elements in the settings section
+// Checkbox input elements in the settings section
 const checkboxes = qsa('.setting-checkbox');
 
 // Constant values for the game board grid
@@ -36,6 +36,15 @@ const NUM_OF_ROW = 6;
 
 // Stores input values from the settings section form
 let playerData;
+
+// Current player. The value changes after a player makes a move.
+let player1Turn = true;
+
+// Total available cell. The number decreases after a player places a disc.
+let freeCellCounter = 42;
+
+// The game status. The game loop is running while the value is false.
+let gameOver = false;
 
 // Button Click EventListeners
 logoLink.addEventListener('click', () => {
@@ -62,6 +71,7 @@ menuLeaderboardBtn.addEventListener('click', () => {
 settingStartBtn.addEventListener('click', (e) => {
   e.preventDefault();
   startBtnHandler();
+  openSection('game');
 });
 settingCloseBtn.addEventListener('click', (e) => {
   e.preventDefault();
@@ -102,30 +112,31 @@ footerContactBtn.addEventListener('click', () => {
 });
 
 // Checkbox change EventListener
-checkboxes.forEach(checkbox => 
+checkboxes.forEach(checkbox =>
   checkbox.addEventListener('change', invalidChangeHandler));
 
 /**
- * Checks input values from the settings section form and
- * calls functions that creates the game grid, clears it and opens the game section respectively.
+ * Checks input values from the settings section form,
+ * sets player's name if empty and
+ * assigns the values to the playerData object.
  */
 function startBtnHandler() {
   let player1Type = elById('player1Type').checked ? 'human' : 'computer';
   let player1Colour = elById('player1Colour').checked ? 'red' : 'yellow';
   let player1Name = elById('player1Name').value.trim();
-  let player2Type = elById('player2Type') ? 'human' : 'computer';
+  let player2Type = elById('player2Type').checked ? 'human' : 'computer';
   let player2Colour = elById('player2Colour').checked ? 'red' : 'yellow';
   let player2Name = elById('player2Name').value.trim();
 
-  if ( player1Type === 'computer' && player2Type === 'human' ) {
+  if (player1Type === 'computer' && player2Type === 'human') {
     player1Name = player1Name === '' ? 'Computer' : player1Name;
     player2Name = player2Name === '' ? 'Player' : player2Name;
   }
-  if ( player1Type === 'human' && player2Type === 'computer' ) {
+  if (player1Type === 'human' && player2Type === 'computer') {
     player1Name = player1Name === '' ? 'Player' : player1Name;
     player2Name = player2Name === '' ? 'Computer' : player2Name;
   }
-  if ( player1Type === 'human' && player2Type === 'human' ) {
+  if (player1Type === 'human' && player2Type === 'human') {
     player1Name = player1Name === '' ? player1Colour : player1Name;
     player2Name = player2Name === '' ? player2Colour : player2Name;
   }
@@ -139,9 +150,39 @@ function startBtnHandler() {
     player2Name
   }
 
+  runGame();
+}
+
+/**
+ * Creates and configures a new game.
+ */
+function runGame() {
   createGrid();
-  clearGrid();
-  openSection('game');
+
+  updateName(player1Turn ? playerData.player1Name : playerData.player2Name);
+  updateColour(player1Turn ? playerData.player1Colour : playerData.player2Colour);
+
+  if (playerData.player1Type === 'human' && playerData.player2Type === 'human') {
+    multiPlayerGame();
+  } else {
+    singlePlayerGame();
+  }
+}
+
+function updateName(currentPlayerName) {
+
+}
+
+function updateColour(currentPlayerColour) {
+
+}
+
+function multiPlayerGame() {
+
+}
+
+function singlePlayerGame() {
+
 }
 
 /**
@@ -151,12 +192,12 @@ function startBtnHandler() {
 function createGrid() {
   const boardEl = elById('boardGrid');
 
-  for ( let i = 0; i < NUM_OF_ROW + 1; i++ ) {
-    for ( let j = 0; j < NUM_OF_COLUMN; j++ ) {
+  for (let i = 0; i < NUM_OF_ROW + 1; i++) {
+    for (let j = 0; j < NUM_OF_COLUMN; j++) {
       const cell = document.createElement('div');
       cell.className = 'cell';
 
-      if ( i === 0 ) {
+      if (i === 0) {
         cell.setAttribute('tabindex', 0);
         cell.className += ' invisible';
       } else {
@@ -177,9 +218,9 @@ function clearGrid() {
  * at least one player must be a human and
  * each player must have a different colour.
  */
- function invalidChangeHandler() {
-  // checkboxes[0] and checkboxes[2] are for the player type. If unchecked, it's a computer.
-  if ( !checkboxes[0].checked && !checkboxes[2].checked ) {
+function invalidChangeHandler() {
+
+  if (!elById('player1Type').checked && !elById('player2Type').checked) {
     const overlay = document.createElement('div');
     overlay.className = 'overlay';
     overlay.innerHTML = `
@@ -191,8 +232,7 @@ function clearGrid() {
     settingsSection.appendChild(overlay);
   }
 
-  // checkboxes[1] and checkboxes[3] are for the player colour. If unchecked, it's yellow.
-  if ( checkboxes[1].checked === checkboxes[3].checked ) {
+  if (elById('player1Colour').checked === elById('player2Colour').checked) {
     const overlay = document.createElement('div');
     overlay.className = 'overlay';
     overlay.innerHTML = `
@@ -214,11 +254,11 @@ function confirmSetting(type) {
   const overlays = qsa('.overlay');
   overlays.forEach(overlay => overlay.remove());
 
-  if ( type === 'player' ) {
+  if (type === 'player') {
     checkboxes[0].checked = false;
     checkboxes[2].checked = true;
   }
-  if ( type === 'colour' ) {
+  if (type === 'colour') {
     checkboxes[1].checked = false;
     checkboxes[3].checked = true;
   }
@@ -231,20 +271,22 @@ function confirmSetting(type) {
  */
 function openSection(name) {
 
-  for ( i = 0; i < sections.length; i++ ) {
-    if ( i === 1 ) { continue; }
+  for (i = 0; i < sections.length; i++) {
+    if (i === 1) {
+      continue;
+    }
     sections[i].classList.remove('active');
   }
 
-  if ( name === 'settings' ) {
+  if (name === 'settings') {
     sections[0].classList.add('active');
-  } else if ( name === 'game' ) {
+  } else if (name === 'game') {
     sections[1].classList.add('active');
-  } else if ( name === 'help' ) {
+  } else if (name === 'help') {
     sections[2].classList.add('active');
-  } else if ( name === 'leaderboard' ) {
+  } else if (name === 'leaderboard') {
     sections[3].classList.add('active');
-  } else if ( name === 'contact' ) {
+  } else if (name === 'contact') {
     sections[4].classList.add('active');
   } else {
     throw `Invalid section name: ${name}. Aborting!`;
@@ -255,8 +297,10 @@ function openSection(name) {
  * Closes all the sections except the game section
  */
 function closeSection() {
-  for ( i = 0; i < sections.length; i++ ) {
-    if ( i === 1 ) { continue; }
+  for (i = 0; i < sections.length; i++) {
+    if (i === 1) {
+      continue;
+    }
     sections[i].classList.remove('active');
   }
 }
